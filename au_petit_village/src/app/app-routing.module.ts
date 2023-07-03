1e659570f5f14942ffb1f60617bce536e873bf99
Kevin Rognon <rognonk@gmail.com>
2023-07-03 17:36:50 +0200

    Mise à jour

--- au_petit_village/src/app/app-routing.module.ts
+++ au_petit_village/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {ProduitsComponent} from "./produits/produits.component";
+import {ContactComponent} from "./contact/contact.component";
 
 const routes: Routes = [
-  {path: "", component: HomeComponent}
+  {path: "", component: HomeComponent},
+  {path: "produits", component: ProduitsComponent},
+  {path: "contact", component: ContactComponent},
 ];
 
 @NgModule({
--- au_petit_village/src/app/app.module.ts
+++ au_petit_village/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProduitsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
