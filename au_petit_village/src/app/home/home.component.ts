7aa1fe331e741f1fe4fb7ca1efa67727e3374f8d
Kevin Rognon <rognonk@gmail.com>
2023-07-04 18:59:39 +0200

    Mise à jour
    Ajout des dynamismes de produits sur la page d'accueil.

--- au_petit_village/src/app/home/home.component.ts
+++ au_petit_village/src/app/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-import 'src/assets/db_products/data.json';
+import {Component, OnInit} from '@angular/core';
+import {ProductsService} from "../products.service";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  produits: any[] = [];
+  constructor(private productsService: ProductsService) {
+  }
+
+  ngOnInit() {
+    this.produits = this.productsService.dataProducts;
+  }
 }
---
+++ au_petit_village/src/assets/db_products/data.ts
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    "id": 0,
+    "product_name": "Obélix",
+    "product_desc": "Figurine d'Obélix, les mains dans les poches.",
+    "product_price": 29.99,
+    "product_img": "../assets/images/produits/obelix.jpg"
+  },
+  {
+    "id": 1,
+    "product_name": "Astérix",
+    "product_desc": "Figurine d'Astérix avec son casque ailé.",
+    "product_price": 29.99,
+    "product_img": "../assets/images/produits/asterix.jpg"
+  },
+  {
+    "id": 2,
+    "product_name": "Idéfix",
+    "product_desc": "Figurine d'Idéfix, le petit chien d'Astérix.",
+    "product_price": 29.99,
+    "product_img": "../assets/images/produits/idefix.jpg"
+  },
+  {
+    "id": 3,
+    "product_name": "Abraracourcix",
+    "product_desc": "Figurine d'Abraracourcix, le chef du village gaulois.",
+    "product_price": 19.99,
+    "product_img": "../assets/images/produits/abraracourcix.jpg"
+  },
+  {
+    "id": 4,
+    "product_name": "Falbala",
+    "product_desc": "Figurine de Falbala, la belle Gauloise.",
+    "product_price": 16.99,
+    "product_img": "../assets/images/produits/falbala.jpg"
+  }
+
+]
