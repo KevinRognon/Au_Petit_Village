e9e4ddec0e8e8b77e6a469aa934cc3ad63800c54
Kevin Rognon <rognonk@gmail.com>
2023-07-05 13:34:31 +0200

    Mise à jour

--- au_petit_village/src/app/home/home.component.ts
+++ au_petit_village/src/app/home/home.component.ts
@@ -1,27 +1,35 @@
-import {Component, OnInit} from '@angular/core';
-import {ProductsService} from "../products.service";
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit{
-
+export class HomeComponent implements OnInit {
   produits: any[] = [];
   searchText = '';
-  filteredProduits : any[] = [];
-  constructor(private productsService: ProductsService) {
-  }
+  filteredProduits: any[] = [];
+  responseText: string = ''; // Modifiez le type de la variable
+
+  constructor(private productsService: ProductsService) {}
 
   filterProduits() {
     this.filteredProduits = this.produits.filter(produit => {
-      return produit.product_name.toLowerCase().includes(this.searchText.toLowerCase());
-    })
+      const isMatching = produit.product_name.toLowerCase().includes(this.searchText.toLowerCase());
+      if(isMatching) {
+        this.responseText = '';
+        return true;
+      }
+      else {
+        this.responseText = 'Aucun produit correspondant';
+        return false;
+      }
+    });
   }
 
   ngOnInit() {
-    this.produits = this.productsService.dataProducts
+    this.produits = this.productsService.dataProducts;
     this.filterProduits();
   }
-
 }
